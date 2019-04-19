1313917fcd830be889d7c1a9ab38d19029b3f1d5
Xavier Gillmann <xgi0101@gmail.com>
2019-04-19 16:20:05 +0200

    chmod on libmsodbcsql-17.3.so.1.1?

---
+++ modules-tests.py
@@ -0,0 +1,22 @@
+import os
+import json
+import sys
+import re
+
+def load_openimis_conf():
+    conf_file_path = sys.argv[1]
+    if not conf_file_path:
+        sys.exit("Missing config file path argument")
+    if not os.path.isfile(conf_file_path):
+        sys.exit("Config file parameter refers to missing file %s" % conf_file_path)
+
+    with open(conf_file_path) as conf_file:
+        return json.load(conf_file)
+
+def extract_test(module):
+    return "python manage.py test %s -n" % re.split('[^a-zA-Z0-9\-_]',module["pip"])[0]
+
+OPENIMIS_CONF = load_openimis_conf()
+MODULES = list(map(extract_test, OPENIMIS_CONF["modules"]))
+print("#!/bin/bash")
+print("\n".join(MODULES))
