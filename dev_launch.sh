b475a71bf8cc94ac8025178fdf55df258f86e0f9
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-10-10 22:17:09 +0200

    review 20191011

--- dev_launch.sh
+++ dev_launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source venv/bin/activate
 cd openIMIS
-SITE_ROOT=api REMOTE_USER_AUTHENTICATION=True DEBUG=True ROW_SECURITY=False python manage.py runserver
+SITE_ROOT=api REMOTE_USER_AUTHENTICATION=True DEBUG=True python manage.py runserver
