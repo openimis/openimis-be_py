80d1580fb93112b12f3cbfa1a91ce32532fc8684
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-10-28 11:09:46 +0100

    amqp via ENV

--- openIMIS/openIMIS/settings.py
+++ openIMIS/openIMIS/settings.py
@@ -178,7 +178,7 @@ DATABASES = {
 }
 
 # Celery message broker configuration for RabbitMQ. One can also use Redis on AWS SQS
-CELERY_BROKER_URL = "amqp://127.0.0.1"
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "amqp://127.0.0.1")
 
 AUTH_USER_MODEL = 'core.User'
 
