14e71fa808908fc3b7f3cc749dc3509346f9a869
Patrick Delcroix <pmpdelcroix@gmail.com>
2020-04-25 21:08:46 +0200

    add -UseBasicParsing for iwr

--- script/healthcheck.ps1
+++ script/healthcheck.ps1
@@ -1,5 +1,5 @@
 try { 
-        $response = iwr http://localhost:8000/ht/?format=json -TimeoutSec 3;
+        $response = iwr http://localhost:8000/ht/?format=json -TimeoutSec 3  -UseBasicParsing;
         if ($response.StatusCode -eq 200) { 
             exit 0 
         } else { 
