ecd76fdfc6d58668d9a8463c2ca3a05fa08fe032
Eric Darchis <darchis@pobox.com>
2020-09-29 15:40:45 +0200

    Schema logging strongly improved + log config
    The dynamic schema loading tended to be touchy. When a module had a compilation error, it would often be silently ignored, resulting, at best, in incomplete GraphQL schemas.
    I have added lots of logs in DEBUG and a sample entry in the settings file to display them.
    Also took the opportunity to improve the sample logging configuration to make debugging easier and show what can be done.

--- openIMIS/openIMIS/schema.py
+++ openIMIS/openIMIS/schema.py
@@ -2,6 +2,10 @@ import graphene
 from .openimisapps import openimis_apps
 from graphene_django.debug import DjangoDebug
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 all_apps = openimis_apps()
 queries = []
 mutations = []
@@ -15,22 +19,34 @@ for app in all_apps:
         schema = __import__(f"{app}.schema")
         if hasattr(schema.schema, "Query"):
             queries.append(schema.schema.Query)
+            logger.debug(f"{app} queries loaded")
         if hasattr(schema.schema, "bind_signals"):
             bind_signals.append(schema.schema.bind_signals)
+            logger.debug(f"{app} signals bound")
     except ModuleNotFoundError as exc:
-        pass  # The module doesn't have a schema.py, just skip
+        # The module doesn't have a schema.py, just skip
+        logger.debug(f"{app} has no schema module, skipping")
     except AttributeError as exc:
+        logger.debug(f"{app} queries couldn't be loaded")
         raise  # This can be hiding actual compilation errors
+    except Exception as exc:
+        logger.debug(f"{app} exception", exc)
 
     try:
-        if __import__(f"{app}.schema").schema.Mutation:
-            mutation = __import__(f"{app}.schema").schema.Mutation
+        schema_module = __import__(f"{app}.schema")
+        if hasattr(schema_module.schema, "Mutation"):
+            mutation = schema_module.schema.Mutation
             if mutation:
                 mutations.append(mutation)
+                logger.debug(f"{app} mutations loaded")
+        else:
+            logger.debug(f"{app} has a schema module but no Mutation class")
     except ModuleNotFoundError as exc:
-        pass  # The module doesn't have a schema.py, just skip
+        # The module doesn't have a schema.py, just skip
+        logger.debug(f"{app} has no schema module (mutation), skipping")
     except AttributeError as exc:
-        pass  # The module doesn't have a Query or Mutation, just ignore
+        # The module doesn't have a Query or Mutation, just ignore
+        logger.debug(f"{app} has a schema module and Mutation but failed nonetheless")
 
     for binder in bind_signals:
         binder()
