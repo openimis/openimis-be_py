4c01f8fc77c066ea04170c8e591ec997d80c8159
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2020-12-22 23:56:12 +0100

    correct typo

--- openIMIS/openIMIS/openimisconf.py
+++ openIMIS/openIMIS/openimisconf.py
@@ -3,6 +3,6 @@ import os
 
 
 def load_openimis_conf():
-    conf_file_path = os.environ.get("SITE_ROOT", "../openimis.json")
+    conf_file_path = os.environ.get("OPENIMIS_CONF", "../openimis.json")
     with open(conf_file_path) as conf_file:
         return json.load(conf_file)
