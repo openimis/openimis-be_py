9997a3c51442d5c2ff51e9bdf08a8045394ad52e
sniedzielski <sniedzielski@soldevelo.com>
2021-06-21 10:37:40 +0200

    OE0-2: added description of function to process error '401'

--- openIMIS/openIMIS/rest_exception_handler.py
+++ openIMIS/openIMIS/rest_exception_handler.py
@@ -2,6 +2,11 @@ from rest_framework import exceptions, status, views
 
 
 def fhir_rest_api_exception_handler(exc, context):
+    """
+        function to handle AuthenticationFailed exceptions
+        with returning HTTP 401 in case of such errors.
+    """
+
     response = views.exception_handler(exc, context)
 
     if isinstance(exc, (exceptions.AuthenticationFailed, exceptions.NotAuthenticated)):
