460fba026c946497161c0bd9ffbed873e2bcf045
Marco Pereira <marcopereira@gmx.ch>
2021-07-22 13:14:53 +0200

    conf incoming as a content string instead of path to file

--- openIMIS/openIMIS/openimisconf.py
+++ openIMIS/openIMIS/openimisconf.py
@@ -5,9 +5,8 @@ import os
 def load_openimis_conf():
     conf_json_env = os.environ.get("OPENIMIS_CONF_JSON", "")
     conf_file_path = os.environ.get("OPENIMIS_CONF", "../openimis.json")
-    if conf_json_env:
-        with open(conf_json_env) as conf_file:
-            return json.load(conf_file)
-    else: 
+    if not conf_json_env:
         with open(conf_file_path) as conf_file:
             return json.load(conf_file)
+    else: 
+        return json.load(conf_json_env)
