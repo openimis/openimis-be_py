a916d8eef767ee29485cb6b9d0df940215cfe233
Marco Pereira <marcopereira@gmx.ch>
2021-07-22 16:08:57 +0200

    cleaned file from print statements

--- openIMIS/openIMIS/openimisconf.py
+++ openIMIS/openIMIS/openimisconf.py
@@ -6,10 +6,8 @@ def load_openimis_conf():
     conf_json_env = os.environ.get("OPENIMIS_CONF_JSON", "")
     conf_file_path = os.environ.get("OPENIMIS_CONF", "../openimis.json")
     if not conf_json_env:
-        print("not using local conf...")
         with open(conf_file_path) as conf_file:
             return json.load(conf_file)
     else:
-        conf_json_env = io.StringIO(conf_json_env) 
-        print("using local conf....:")
+        conf_json_env = io.StringIO(conf_json_env)
         return json.load(conf_json_env)
