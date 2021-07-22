68069483f59549cf099dabb75e13486896320e8e
Marco Pereira <marcopereira@gmx.ch>
2021-07-22 08:59:14 +0200

    added print for debug purposes

--- openIMIS/openIMIS/openimisconf.py
+++ openIMIS/openIMIS/openimisconf.py
@@ -4,6 +4,7 @@ import os
 
 def load_openimis_conf():
     conf_json_env = os.environ.get("OPENIMIS_CONF_JSON", "")
+    print(conf_json_env)
     conf_file_path = os.environ.get("OPENIMIS_CONF", "../openimis.json")
     if conf_json_env:
         with open(conf_json_env) as conf_file:
