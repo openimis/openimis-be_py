81b6c38b810aca0a65b956c2d6fedd15ea0a345b
Marco Pereira <marcopereira@gmx.ch>
2021-07-22 11:04:30 +0200

    copying conf first

--- openIMIS/openIMIS/openimisconf.py
+++ openIMIS/openIMIS/openimisconf.py
@@ -8,7 +8,7 @@ def load_openimis_conf():
     conf_file_path = os.environ.get("OPENIMIS_CONF", "../openimis.json")
     if conf_json_env:
         with open(conf_json_env) as conf_file:
-            return json.load(conf_file)
+            return json.load("../openimis_local.json")
     else:
         with open(conf_file_path) as conf_file:
             return json.load(conf_file)
