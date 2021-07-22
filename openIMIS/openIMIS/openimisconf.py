793f1699660c13d487a8e804643f232863b19928
Marco Pereira <33976946+magp18@users.noreply.github.com>
2021-07-22 11:26:59 +0200

    Update openimisconf.py

--- openIMIS/openIMIS/openimisconf.py
+++ openIMIS/openIMIS/openimisconf.py
@@ -6,9 +6,5 @@ def load_openimis_conf():
     conf_json_env = os.environ.get("OPENIMIS_CONF_JSON", "")
     print(conf_json_env)
     conf_file_path = os.environ.get("OPENIMIS_CONF", "../openimis.json")
-    if conf_json_env:
-        with open(conf_file_path) as conf_file:
-            return json.load(conf_file)
-    else:
-        with open(conf_file_path) as conf_file:
-            return json.load(conf_file)
+    with open(conf_file_path) as conf_file:
+        return json.load(conf_file)
