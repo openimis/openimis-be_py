b4929ef7588145c02374ef1c300eb5a56434a945
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2022-03-31 15:01:42 +0200

    updated dev launch

--- dev_launch.sh
+++ dev_launch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 source venv/bin/activate
+pip install -r requirements.txt 
+python modules-requirements.py openimis.json > modules-requirements.txt 
+pip install -r modules-requirements.txt 
 cd openIMIS
+python mnanage.py migrate
 SITE_ROOT=api REMOTE_USER_AUTHENTICATION=False ROW_SECURITY=False DEBUG=True python manage.py runserver
