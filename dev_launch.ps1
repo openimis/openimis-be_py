8e8684478e86ea81d2df7ae02834b0b94d8b99c7
Patrick Delcroix <delcroip@gmail.com>
2022-05-30 13:31:18 +0200

    add all env vars

--- dev_launch.ps1
+++ dev_launch.ps1
@@ -1,8 +1,22 @@
 # assuming venv and openimis-be_py folders are on the same level
 ..\venv\Scripts\Activate.ps1
 cd openIMIS
-[Environment]::SetEnvironmentVariable("SITE_ROOT", "api")
-[Environment]::SetEnvironmentVariable("REMOTE_USER_AUTHENTICATION", "True")
+$SITE_ROOT='iapi'
+$DB_NAME='IMISfs'
+$DB_USER='IMISuser'
+$DB_PASSWORD='IMISuser@1234'
+$DB_HOST='127.0.0.1'
+$DB_PORT='1433'
+$DJANGO_PORT='8000'
+[Environment]::SetEnvironmentVariable("SITE_ROOT", $SITE_ROOT)
+[Environment]::SetEnvironmentVariable("REMOTE_USER_AUTHENTICATION", "False")
 [Environment]::SetEnvironmentVariable("ROW_SECURITY", "False")
 [Environment]::SetEnvironmentVariable("DEBUG", "True")
+[Environment]::SetEnvironmentVariable("DB_NAME", $DB_NAME)
+[Environment]::SetEnvironmentVariable("DB_USER", $DB_USER)
+[Environment]::SetEnvironmentVariable("DB_PASSWORD", $DB_PASSWORD)
+[Environment]::SetEnvironmentVariable("DB_HOST", $DB_HOST)
+[Environment]::SetEnvironmentVariable("DB_PORT", $DB_PORT)
+[Environment]::SetEnvironmentVariable("DJANGO_PORT", $DJANGO_PORT)
+[Environment]::SetEnvironmentVariable("OPENIMIS_CONF", "../"$OPENIMIS_CONF)
 python manage.py runserver
