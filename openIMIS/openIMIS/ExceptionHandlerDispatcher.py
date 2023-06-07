ff597fd6691ea8fe94f710f9b9097e2bbfae7440
wzglinieckisoldevelo <98958634+wzglinieckisoldevelo@users.noreply.github.com>
2023-06-07 14:51:57 +0200

    Update ExceptionHandlerDispatcher.py
    Update content of this file to be the same as the one on the develop branch.

--- openIMIS/openIMIS/ExceptionHandlerDispatcher.py
+++ openIMIS/openIMIS/ExceptionHandlerDispatcher.py
@@ -1,6 +1,6 @@
 from .ExceptionHandlerRegistry import ExceptionHandlerRegistry
 from rest_framework.views import exception_handler
-from rest_framework import exceptions, status, views
+from rest_framework import exceptions, status
 
 
 def dispatcher(exc, context):
@@ -9,23 +9,16 @@ def dispatcher(exc, context):
     """
     module_name = _extract_module_name(context['request'])
     handler = ExceptionHandlerRegistry.get_exception_handler(module_name)
-
-    response = _process_exception_handler(exc, context)
-    if response:
-        return response
-
+    
     if handler is None:
         # Fallback to default DRF exception handler if no handler is defined for the module
         handler = exception_handler
-    return handler(exc, context)
-
-
-def _process_exception_handler(exc, context):
-    response = views.exception_handler(exc, context)
-
+    
+    response = handler(exc, context)
     if isinstance(exc, (exceptions.AuthenticationFailed, exceptions.NotAuthenticated)):
         response.status_code = status.HTTP_401_UNAUTHORIZED
-        return response
+        
+    return response
 
 
 def _extract_module_name(request):
