a8a459fb68083fa6172d23f978d60ba7393f8669
delcroip <patrick.delcroix@swisstph.ch>
2024-01-29 14:54:43 +0100

    fix default

--- openIMIS/openIMIS/settings.py
+++ openIMIS/openIMIS/settings.py
@@ -288,17 +288,13 @@ DB_DEFAULT = os.environ.get("DB_DEFAULT", 'PSQL')
 
 if os.environ.get("NO_DATABASE", "False") == "True":
     
-    DATABASES = {
-       {
-                'default': {
-                    'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-                    'NAME': ' ../script/sqlite.db',                      # Or path to database file if using sqlite3.
-                    'USER': '',                      # Not used with sqlite3.
-                    'PASSWORD': '',                  # Not used with sqlite3.
-                    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-                    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-                }
-            }
+    DATABASES['default'] = {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': ' ../script/sqlite.db',                      # Or path to database file if using sqlite3.
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3. 
     }
 elif os.environ.get("DB_ENGINE", "False") != "False":
     if "DB_OPTIONS" in os.environ:
