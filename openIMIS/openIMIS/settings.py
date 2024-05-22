3be0f0b6f7e49d34210a20699b67d0c01fa05292
lruzicki <lukasz.ruz@gmail.com>
2024-05-22 09:47:47 +0200

    Cleared options for auth_password_validators

--- openIMIS/openIMIS/settings.py
+++ openIMIS/openIMIS/settings.py
@@ -470,18 +470,6 @@ AUTH_USER_MODEL = "core.User"
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
 ]
 
 
