d35a1250481f94656eb3e40f9c9c4040100dec05
delcroip <patrick.delcroix@swisstph.ch>
2024-05-27 21:41:38 +0200

    deactivate password validator on debug

--- openIMIS/openIMIS/settings.py
+++ openIMIS/openIMIS/settings.py
@@ -470,14 +470,15 @@ AUTH_USER_MODEL = "core.User"
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "core.utils.CustomPasswordValidator",
-    },
-]
+if not DEBUG:
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        },
+        {
+            "NAME": "core.utils.CustomPasswordValidator",
+        }
+    ]
 
 
 # Internationalization
