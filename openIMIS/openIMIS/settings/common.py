900ce162bbb549a1317d5d2b21dd7732925e38b6
Shahzaib Ahmad <shahzaib.ahmad97@gmail.com>
2025-07-29 16:27:46 +0500

    fixed base_dir path so that all subsequent commands will have correct path as before restructuring of settings file

--- openIMIS/openIMIS/settings/common.py
+++ openIMIS/openIMIS/settings/common.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 MODE = os.environ.get("MODE", 'prod').lower()
 if MODE == "dev" or "test" in sys.argv:
